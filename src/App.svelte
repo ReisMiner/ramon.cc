4019886384592fffcbbb869e0310cd86b09527d7
ReisMiner <r1miner@outlook.com>
2022-08-29 20:25:10 +0200

    ez new update

--- src/App.svelte
+++ src/App.svelte
@@ -2,5 +2,30 @@
 </script>
 
 <main>
-  <h1>Hoi</h1>
+    <div>
+        <h1>Hoi</h1>
+        <h1>I'm <strong>Ramon</strong></h1>
+    </div>
+    <h2>This site is still in development!</h2>
 </main>
+
+<style>
+    h1 {
+        margin: 0;
+    }
+    strong{
+        color: coral;
+        font-weight: bold;
+    }
+    div{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+    main {
+        display: grid;
+        align-content: center;
+        height: 100vh;
+    }
+
+</style>
